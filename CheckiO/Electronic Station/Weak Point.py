f88ef6f87abc8cd626fd11883e2e338d07219242
shunyi <M157q.tw@gmail.com>
2015-10-30 22:17:02 +0800

    Weak Point solved.
    Great solution:
    
    + <http://www.checkio.org/mission/weak-point/publications/Sim0000/python-3/first/?ordering=most_voted>
    + <http://www.checkio.org/mission/weak-point/publications/Invertebrate/python-27/short-oneliner/?ordering=most_voted>

---
+++ CheckiO/Electronic Station/Weak Point.py
@@ -0,0 +1,24 @@
+# http://www.checkio.org/mission/weak-point
+
+def weak_point(matrix):
+    rows_durability = [sum(x) for x in matrix]
+    columns_durability = [sum(x) for x in zip(*matrix)]
+    return rows_durability.index(min(rows_durability)), columns_durability.index(min(columns_durability))
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(weak_point([[1]]), (list, tuple)), "The result should be a list or a tuple"
+    assert list(weak_point([[7, 2, 7, 2, 8],
+                            [2, 9, 4, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [3, 3], "Example"
+    assert list(weak_point([[7, 2, 4, 2, 8],
+                            [2, 8, 1, 1, 7],
+                            [3, 8, 6, 2, 4],
+                            [2, 5, 2, 9, 1],
+                            [6, 6, 5, 4, 5]])) == [1, 2], "Two weak point"
+    assert list(weak_point([[1, 1, 1],
+                            [1, 1, 1],
+                            [1, 1, 1]])) == [0, 0], "Top left"
