f61554ce1e28e2a4d457317d02a2a2909b3041e4
shunyi <M157q.tw@gmail.com>
2015-11-04 14:02:58 +0800

    Rotate Hole solved.
    + just do some math in index of the list
        + <http://www.checkio.org/mission/rotate-hole/publications/yukirin/python-3/first/?ordering=most_voted>
    
    Unlocked all sections on CheckiO
    <https://twitter.com/M157q/status/661785637196947457/photo/1>
    \HYPE/

---
+++ CheckiO/Incinerator/Rotate Hole.py
@@ -0,0 +1,12 @@
+# http://www.checkio.org/mission/rotate-hole/
+
+def rotate(state, pipe_numbers):
+    return [i for i in range(len(state)) if all((state[-i:] + state[:-i])[j] for j in pipe_numbers)]
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1]) == [1, 8], "Example"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1, 2]) == [], "Mission impossible"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [0, 4, 5]) == [0], "Don't touch it"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [5, 4, 5]) == [0, 5], "Two cannonballs in the same pipe"
