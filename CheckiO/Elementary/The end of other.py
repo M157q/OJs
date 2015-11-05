0c2e1f195259608fe5a5fed29e03c693dbda4cc3
shunyi <M157q.tw@gmail.com>
2015-11-05 21:47:35 +0800

    The end of other solved.
    + itertools.permutations
        + <http://www.checkio.org/mission/end-of-other/publications/nickie/python-3/set-permutations/#comment-outer-9549>
    + itertools.starmap, itertools.permutations
        + <http://www.checkio.org/mission/end-of-other/publications/veky/python-3/straightforward/?ordering=most_voted>
    + itertools.product
        + <http://www.checkio.org/mission/end-of-other/publications/Uladzimir/python-3/first/?ordering=most_voted>

---
+++ CheckiO/Elementary/The end of other.py
@@ -0,0 +1,12 @@
+# http://www.checkio.org/mission/end-of-other/
+
+def checkio(words_set):
+    return any(any(map(word.endswith, words_set - {word})) for word in words_set)
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio({"hello", "lo", "he"}) == True, "helLO"
+    assert checkio({"hello", "la", "hellow", "cow"}) == False, "hellow la cow"
+    assert checkio({"walk", "duckwalk"}) == True, "duck to walk"
+    assert checkio({"one"}) == False, "Only One"
+    assert checkio({"helicopter", "li", "he"}) == False, "Only end"
