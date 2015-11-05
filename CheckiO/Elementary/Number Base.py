baa72e6ef65d787053332332501883bcab542432
shunyi <M157q.tw@gmail.com>
2015-11-05 23:27:05 +0800

    Number Base solved.
    + more elegant oneliner
        + <http://www.checkio.org/mission/number-radix/publications/nickie/python-3/deja-vu/?ordering=most_voted#comment-37365>
        + `checkio = lambda s, r: (r > int(max(s),36)) - 1 or int(s,r)`
    + use exec() to fit try-except in one line
       + <http://www.checkio.org/mission/number-radix/publications/CounterSparta/python-27/exec/?ordering=most_voted>

---
+++ CheckiO/Elementary/Number Base.py
@@ -0,0 +1,20 @@
+# http://www.checkio.org/mission/number-radix/
+
+# checkio = lambda n, r: int(n, r) if __import__('string').printable.index(max(n.lower())) < r else -1
+
+def checkio(str_number, radix):
+    try:
+        return int(str_number, radix)
+    except ValueError:
+        return -1
+
+
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("101", 2) == 5, "Bin"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("Z", 36) == 35, "Z base"
+    assert checkio("AB", 10) == -1, "B > A > 10"
