195b969bf44a0f171cd8125b4930284fce7fcfd4
shunyi <M157q.tw@gmail.com>
2015-11-07 00:41:43 +0800

    Golden Pyramid solved.
    + functional way, from bottom to up dynamic programming
        + <www.checkio.org/mission/golden-pyramid/publications/review/clear/>

---
+++ CheckiO/Elementary/Golden Pyramid.py
@@ -0,0 +1,43 @@
+# http://www.checkio.org/mission/golden-pyramid/
+
+def count_gold(pyramid):
+    """
+    Return max possible sum in a path from top to bottom
+    """
+
+    if len(pyramid) == 1:
+        return pyramid[0][0]
+
+    return pyramid[0][0] + \
+        max(
+            count_gold(tuple(pyramid[i][:-1] for i in range(1, len(pyramid)))),
+            count_gold(tuple(pyramid[i][1:]  for i in range(1, len(pyramid))))
+        )
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_gold((
+        (1,),
+        (2, 3),
+        (3, 3, 1),
+        (3, 1, 5, 4),
+        (3, 1, 3, 1, 3),
+        (2, 2, 2, 2, 2, 2),
+        (5, 6, 4, 5, 6, 4, 3)
+    )) == 23, "First example"
+    assert count_gold((
+        (1,),
+        (2, 1),
+        (1, 2, 1),
+        (1, 2, 1, 1),
+        (1, 2, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1, 9)
+    )) == 15, "Second example"
+    assert count_gold((
+        (9,),
+        (2, 2),
+        (3, 3, 3),
+        (4, 4, 4, 4)
+    )) == 18, "Third example"
