5acf77150f69ffb8fbb272559bee2b357e50a1e0
shunyi <M157q.tw@gmail.com>
2016-09-12 16:41:06 +0800

    Add solution which accepts both int and float input.
    ref:
    https://py.checkio.org/mission/three-points-circle/publications/M157q/python-3/formula/share/906de0197d27accf55ae3b89c29fcc18/#comment-45222

--- CheckiO/Electronic Station/Three Points Circle.py
+++ CheckiO/Electronic Station/Three Points Circle.py
@@ -1,9 +1,11 @@
 # http://www.checkio.org/mission/three-points-circle/
 
 import math
+import re
 
 def checkio(data):
-    x1, y1, x2, y2, x3, y3 = map(int, filter(str.isdigit, data))
+    # x1, y1, x2, y2, x3, y3 = map(int, filter(str.isdigit, data))
+    x1, y1, x2, y2, x3, y3 = map(float, re.findall(r'(\d+(?:\.\d+)?)', data))
 
     # central point of circle (p, q)
     p = ((y1 - y3)*(y1**2 - y2**2 + x1**2 - x2**2) - (y1 - y2)*(y1**2 - y3**2 + x1**2 - x3**2)) / (2*((y1 - y3)*(x1 - x2) - (y1 - y2)*(x1 - x3)))
@@ -15,4 +17,4 @@ def checkio(data):
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio("(2,2),(6,2),(2,6)") == "(x-4)^2+(y-4)^2=2.83^2"
-    assert checkio("(3,7),(6,9),(9,7)") == "(x-6)^2+(y-5.75)^2=3.25^2"
+    assert checkio("(3.0,7.0),(6.0,9.0),(9.0,7.0)") == "(x-6)^2+(y-5.75)^2=3.25^2"
