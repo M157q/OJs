5d306702eb0dd1fd408be9339fae8d8895a27fbe
shunyi <M157q.tw@gmail.com>
2017-04-25 01:25:10 +0800

    [HackerRank] Solved "Day 1: Data Types"

---
+++ HackerRank/All Domains/Tutorials/30 Days of Code/Day 1: Data Types.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/30-data-types
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+j = int(input())
+e = float(input())
+t = input()
+
+print(i+j)
+print(d+e)
+print(s+t)
